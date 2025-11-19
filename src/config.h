35bd72a319e26521a8a932d2c7ea3db99d119ccf
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-11-19 19:51:45 +0000

    Updates CML and config.

--- src/config.h
+++ src/config.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #define GLFW_INCLUDE_VULKAN
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define _USE_MATH_DEFINES
 
 #define GRAPHICS_API_VULKAN
 #define TRACE_INFO_LOG true
